cb9e75f600dc0b7c117efdf2c44703fcacfe92fe
maksim-soldatenko <maksim.soldatenko@gmail.com>
2017-07-25 03:23:47 +0300

    demo added

---
+++ SecurityLecture/Demo/Local/LocalAuthorization/LocalAuthorization/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LocalAuthorization.MvcApplication" Language="C#" %>
---
+++ SecurityLecture/Demo/Local/LocalAuthorization/LocalAuthorization/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocalAuthorization.Startup))]
+namespace LocalAuthorization
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
