074d57c4edb70b19d4b8c44faee71f8a74c513f2
Maksim <Soldatenko>
2017-07-31 21:45:11 +0300

    LoggingLecture added

---
+++ LoggingLecture/Demo/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Common.Logging;
+using log4net.Config;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILog _log= LogManager.GetLogger(typeof(HomeController));
+        public ActionResult Index()
+        {
+            _log.Debug("Index called");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+
+            _log.Debug("About called");
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            ViewBag.Message = "Your contact page.";
+
+            FormatMessageHandler format =
+                (s, args) => $"Formatted exception: Message:{s}. Details: {args[0]}, some words: {args[1]}, UserId: {args[2]}";
+
+            _log.Fatal(format("Fatal error", "args0", "args1", "User Vasia"));
+
+            return View();
+        }
+    }
+}
